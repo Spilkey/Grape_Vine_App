fe440a8beb3042fbf67dc84966f43edd13983e99
Matt <kermitbadaboom@gmail.com>
2020-11-10 20:29:19 -0500

    Added function to get posts from database under specific topic id

---
+++ final_project/lib/models/topic-model.dart
@@ -0,0 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:final_project/models/db.dart';
+
+class TopicModel {
+
+  // grabs data from firebase under collection 'posts' and the given id
+  Stream<QuerySnapshot> getPostsFromTopic(topic_id)  {
+    FirebaseFirestore db = DB().database;
+    Stream<QuerySnapshot> results = db
+      .collection('posts')
+      .where('topic_id', isEqualTo: topic_id)
+      .snapshots();
+    return results;
+  }
+}
