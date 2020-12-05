4784d0e976a8feee1204a57585e58fd39da79c7e
etienne <etienne.caronan@uoit.net>
2020-12-04 23:10:49 -0500

    load profile data, post data and refactoring of code

--- final_project/lib/views/profile_page.dart
+++ final_project/lib/views/profile_page.dart
@@ -1,4 +1,5 @@
 import 'dart:typed_data';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../components/profile_feed_card.dart';
 
@@ -9,12 +10,8 @@ class ProfilePage extends StatefulWidget {
     Key key,
   }) : super(key: key);
 
-  // TODO: uncomment once profile page is linked to actual accounts
   final String userName;
-  // final String bio;
   final Uint8List userImage;
-  // final bool personAdded;
-  // final List<?> userPosts;
 
   @override
   _ProfilePageState createState() => _ProfilePageState();
@@ -24,152 +21,152 @@ class _ProfilePageState extends State<ProfilePage> {
   final double avatarRadius = 50, marginTop = 50;
   final double profileContainerSize = 0.4;
 
+  CollectionReference userProfile =
+      FirebaseFirestore.instance.collection('users');
+
+  CollectionReference userPosts =
+      FirebaseFirestore.instance.collection('posts');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              stops: [
-            0.4,
-            0.4,
-            0.2,
-            0.7,
-            0.1
-          ],
-              colors: [
-            // Colors are easy thanks to Flutter's Colors class.
-            Color(0xFF6541A5),
-            Color(0xFF4D307F),
-            Color(0xFF472B75),
-            Color(0xFF3D2464),
-            Color(0xFF2C1745),
-          ])),
-      child: Column(
-        children: [
-          Container(
-              child: SizedBox(
-                  height:
-                      MediaQuery.of(context).size.height * profileContainerSize,
-                  child: Column(
-                    children: [
-                      // ACTION BUTTONS
-                      Container(
-                          margin: EdgeInsets.only(top: 10),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              IconButton(
-                                  icon: Icon(
-                                    Icons.arrow_back,
-                                    color: Colors.deepPurpleAccent,
-                                  ),
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  }),
-                              // TODO: change the appearance once added
-                              IconButton(
-                                  icon: Icon(
-                                    Icons.person_add_alt,
-                                    color: Colors.deepPurpleAccent,
-                                  ),
-                                  onPressed: () {})
-                            ],
-                          )),
-                      // USER AVATAR
-                      Center(
-                          child: widget.userImage == null
-                              ? CircleAvatar(
-                                  backgroundColor: Colors.deepPurple,
-                                  radius: avatarRadius,
-                                  child: Icon(Icons.person))
-                              : CircleAvatar(
-                                  radius: avatarRadius,
-                                  backgroundImage:
-                                      MemoryImage(widget.userImage),
+      decoration: BoxDecoration(gradient: backgroundGradient()),
+      child: FutureBuilder(
+          // TODO: to replace with widget.username
+          future:
+              userProfile.where('user_id', isEqualTo: 'temp_new_post_id').get(),
+          builder: (context, snapshot) {
+            var userProfileData = snapshot.data.docs[0].data();
+
+            return Column(
+              children: [
+                Container(
+                    child: SizedBox(
+                        height: MediaQuery.of(context).size.height *
+                            profileContainerSize,
+                        child: Column(
+                          children: [
+                            // ACTION BUTTONS
+                            Container(
+                                margin: EdgeInsets.only(top: 10),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    IconButton(
+                                        icon: Icon(
+                                          Icons.arrow_back,
+                                          color: Colors.deepPurpleAccent,
+                                        ),
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        }),
+                                    // TODO: change the appearance once added
+                                    IconButton(
+                                        icon: Icon(
+                                          Icons.person_add_alt,
+                                          color: Colors.deepPurpleAccent,
+                                        ),
+                                        onPressed: () {})
+                                  ],
                                 )),
-                      Container(
-                          margin: EdgeInsets.only(top: 10),
-                          child: Center(
-                            child: Text(
-                              widget.userName,
-                              style: TextStyle(
-                                  color: Colors.deepPurpleAccent,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18),
-                            ),
-                          )),
-                      // TODO: add overflow condition?
-                      // USER BIO
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                        child: Expanded(
-                            child: Text(
-                                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra et quam eget tempus.')),
-                      ),
-                      // USER FRIEND COUNT
-                      Container(
-                          margin: EdgeInsets.only(top: 10),
-                          child: Center(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text('127 ',
+                            // USER AVATAR
+                            Center(
+                                child: widget.userImage == null
+                                    ? CircleAvatar(
+                                        backgroundColor: Colors.deepPurple,
+                                        radius: avatarRadius,
+                                        child: Icon(Icons.person))
+                                    : CircleAvatar(
+                                        radius: avatarRadius,
+                                        backgroundImage:
+                                            MemoryImage(widget.userImage),
+                                      )),
+                            Container(
+                                margin: EdgeInsets.only(top: 10),
+                                child: Center(
+                                  child: Text(
+                                    widget.userName,
                                     style: TextStyle(
                                         color: Colors.deepPurpleAccent,
-                                        fontWeight: FontWeight.bold)),
-                                Text(
-                                  'Friends',
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                )
-                              ],
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
+                                  ),
+                                )),
+                            // USER BIO
+                            Container(
+                              margin: EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 10),
+                              child:
+                                  Expanded(child: Text(userProfileData['bio'])),
                             ),
-                          ))
-                    ],
-                  )),
-              color: Colors.white),
-          // POSTS
-          // TODO: replace this with ListView Builder when integrating DB
-          Container(
-            child: Expanded(
-                child: ListView(
-              children: [
-                FeedCard(
-                  ownerName: 'Owner name',
-                  imageData: '',
-                  ownerProfileImageData: '',
-                  postTitle: 'Title',
-                  postContent: 'Content',
-                ),
-                FeedCard(
-                  ownerName: 'Owner name',
-                  imageData: '',
-                  ownerProfileImageData: '',
-                  postTitle: 'Title',
-                  postContent: 'Content',
-                ),
-                FeedCard(
-                  ownerName: 'Owner name',
-                  imageData: '',
-                  ownerProfileImageData: '',
-                  postTitle: 'Title',
-                  postContent: 'Content',
-                ),
-                FeedCard(
-                  ownerName: 'Owner name',
-                  imageData: '',
-                  ownerProfileImageData: '',
-                  postTitle: 'Title',
-                  postContent: 'Content',
-                )
+                            // USER FRIEND COUNT
+                            Container(
+                                margin: EdgeInsets.only(top: 10),
+                                child: Center(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                          '${userProfileData['friends'].length} ',
+                                          style: TextStyle(
+                                              color: Colors.deepPurpleAccent,
+                                              fontWeight: FontWeight.bold)),
+                                      Text(
+                                        'Friends',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      )
+                                    ],
+                                  ),
+                                )),
+                          ],
+                        )),
+                    color: Colors.white),
+                // POSTS
+                FutureBuilder(
+                    future: userPosts
+                        .where('owner_id', isEqualTo: 'temp_new_post_id')
+                        .get(),
+                    builder: (context, snapshot) {
+                      return Container(
+                        child: Expanded(child:
+                            ListView.builder(itemBuilder: (context, index) {
+                          var postData = snapshot.data.docs[index].data();
+                          return FeedCard(
+                            ownerName: 'temp_new_post_id',
+                            imageData: postData['image_data'],
+                            ownerProfileImageData: postData['post_image_data'],
+                            postTitle: postData['post_title'],
+                            postContent: postData['content'],
+                          );
+                        })),
+                      );
+                    }),
               ],
-            )),
-          ),
-        ],
-      ),
+            );
+          }),
     ));
   }
+}
+
+LinearGradient backgroundGradient() {
+  return LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      stops: [
+        0.4,
+        0.4,
+        0.2,
+        0.7,
+        0.1
+      ],
+      colors: [
+        Color(0xFF6541A5),
+        Color(0xFF4D307F),
+        Color(0xFF472B75),
+        Color(0xFF3D2464),
+        Color(0xFF2C1745),
+      ]);
 }
