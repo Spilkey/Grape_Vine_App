c29273d5da859380081c30e961f55df6eddcf2ca
etienne <etienne.caronan@uoit.net>
2020-12-09 00:26:15 -0500

    updated ui to include datatable

--- final_project/lib/views/content_preferences.dart
+++ final_project/lib/views/content_preferences.dart
@@ -7,6 +7,21 @@ class ContentPreferences extends StatefulWidget {
 }
 
 class _ContentPreferencesState extends State<ContentPreferences> {
+  // TODO: return preferences object to do settings page so the settings can be passed to the database
+  // TODO: pull from the database all the topics
+  var preferences = [
+    {'topic': 'Celebrities', 'selected': false},
+    {'topic': 'Movies', 'selected': false},
+    {'topic': 'Music', 'selected': false},
+    {'topic': 'Technology', 'selected': false},
+    {'topic': 'Travel', 'selected': false},
+    {'topic': 'Gaming', 'selected': false},
+    {'topic': 'Art', 'selected': false},
+    {'topic': 'Food', 'selected': false},
+    {'topic': 'Politics', 'selected': false},
+    {'topic': 'Sports', 'selected': false},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +31,42 @@ class _ContentPreferencesState extends State<ContentPreferences> {
           leading: IconButton(
               icon: Icon(Icons.arrow_back),
               onPressed: () => Navigator.pop(context))),
-      body: Text('Hello World'),
+      body: SizedBox(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+              child: Text(
+                  'These are your content preferences. You can manage which topics you see on the Discovery page.'),
+            ),
+            DataTable(
+              columns: [DataColumn(label: Text('Preferences'))],
+              rows: List<DataRow>.generate(
+                  preferences.length,
+                  (index) => DataRow(
+                        cells: [
+                          DataCell(Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                preferences[index]['topic'],
+                              ),
+                              Checkbox(
+                                  value: preferences[index]['selected'],
+                                  onChanged: (bool value) {
+                                    setState(() {
+                                      preferences[index]['selected'] =
+                                          !preferences[index]['selected'];
+                                    });
+                                  })
+                            ],
+                          )),
+                        ],
+                      )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
