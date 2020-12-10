68e9e531652856621294f2d35346641acbdce9d7
Matt <kermitbadaboom@gmail.com>
2020-12-10 12:57:04 -0500

    deleted unecessary function

--- final_project/lib/models/user_model.dart
+++ final_project/lib/models/user_model.dart
@@ -16,10 +16,7 @@ class UserModel {
     return res;
   }
 
-  updateSubscriptions(User user, String subscription){
-    FirebaseFirestore db = DB().database;
-    var res = db.collection('users').doc(user.id).
-  }
+
 
   Future<User> getUser(String id) async {
     FirebaseFirestore db = DB().database;
