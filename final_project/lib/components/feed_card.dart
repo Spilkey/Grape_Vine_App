10d3ee30fee21f659477013adeec4688c68cc7b7
Spilkey <spilkey1234@gmail.com>
2020-12-10 00:29:49 -0500

    made feed card grab userdata and now have contatn height

--- final_project/lib/components/feed_card.dart
+++ final_project/lib/components/feed_card.dart
@@ -1,5 +1,8 @@
 import 'dart:typed_data';
 import 'dart:convert';
+import 'package:final_project/models/user.dart';
+import 'package:final_project/models/user_model.dart';
+import 'package:final_project/utils/image_utils.dart';
 import 'package:flutter/material.dart';
 import '../views/profile_page.dart';
 
@@ -40,6 +43,12 @@ class FeedCard extends StatefulWidget {
 }
 
 class _FeedCardState extends State<FeedCard> {
+  bool userDataloaded = false;
+
+  User userData;
+
+  UserModel _uModel = new UserModel();
+
   @override
   Widget build(BuildContext context) {
     // Get the data from the db
@@ -48,10 +57,19 @@ class _FeedCardState extends State<FeedCard> {
     bool useDefaultImage;
     Widget location;
 
-    if (!widget.ownerProfileImageData.isEmpty) {
+    if (userData != null) {
+      if (userData.profilePic != null) {
+        useDefaultImage = false;
+        Uint8List _bytesOwnerImage =
+            ImageUtil.getDataFromBase64String(userData.profilePic);
+        profileImage = CircleAvatar(
+          backgroundImage: MemoryImage(_bytesOwnerImage),
+        );
+      }
+    } else if (!widget.ownerProfileImageData.isEmpty) {
       useDefaultImage = false;
       Uint8List _bytesOwnerImage =
-          Base64Codec().decode(widget.ownerProfileImageData);
+          ImageUtil.getDataFromBase64String(widget.ownerProfileImageData);
       profileImage = CircleAvatar(
         backgroundImage: MemoryImage(_bytesOwnerImage),
       );
@@ -60,12 +78,19 @@ class _FeedCardState extends State<FeedCard> {
       profileImage = CircleAvatar(child: Icon(Icons.person));
     }
     if (!widget.imageData.isEmpty) {
-      Uint8List _bytesPostImage = Base64Codec().decode(widget.imageData);
+      Uint8List _bytesPostImage =
+          ImageUtil.getDataFromBase64String(widget.imageData);
       postImage = Container(
-        child: Image.memory(_bytesPostImage),
+        height: 200,
+        child: Image.memory(
+          _bytesPostImage,
+          height: 200,
+        ),
       );
     } else {
-      postImage = Container();
+      postImage = Container(
+        height: 200,
+      );
     }
     if (widget.streetName != null) {
       location = Row(children: <Widget>[
@@ -83,57 +108,86 @@ class _FeedCardState extends State<FeedCard> {
     } else {
       location = Row();
     }
+    if (widget.userId != null && !userDataloaded) {
+      _uModel.getUser(widget.userId).then((value) {
+        userData = value;
 
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: Card(
-        child: Column(
-          children: [
-            ListTile(
-              leading: profileImage,
-              title: GestureDetector(
-                child: Text(widget.ownerName),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        Uint8List userImage = useDefaultImage
-                            ? null
-                            : Base64Codec()
-                                .decode(widget.ownerProfileImageData);
-                        return ProfilePage(
-                          userID: widget.userId,
-                          userImage: userImage,
-                        );
-                      },
-                    ),
-                  );
-                },
-              ),
-            ),
-            Container(
-              child: Text(widget.postTitle),
-            ),
-            location,
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                widget.postContent,
-                style: TextStyle(color: Colors.black.withOpacity(0.6)),
-              ),
-            ),
-            postImage
+        setState(() {
+          userDataloaded = !userDataloaded;
+        });
+      }).catchError((error) {
+        print(error);
+        setState(() {
+          userDataloaded = !userDataloaded;
+        });
+      });
+      return Container(
+        height: 500,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(color: Colors.grey[400], spreadRadius: 3, blurRadius: 10),
           ],
         ),
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(color: Colors.grey[400], spreadRadius: 3, blurRadius: 10),
-        ],
-      ),
-    );
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else {
+      return Container(
+        height: 500,
+        padding: EdgeInsets.all(10),
+        child: Card(
+          child: Column(
+            children: [
+              ListTile(
+                leading: profileImage,
+                title: GestureDetector(
+                  child: Text(
+                      userData != null ? userData.userName : widget.ownerName),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          Uint8List userImage = useDefaultImage
+                              ? null
+                              : Base64Codec()
+                                  .decode(widget.ownerProfileImageData);
+                          return ProfilePage(
+                            userID: widget.userId,
+                            userImage: userImage,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Container(
+                child: Text(widget.postTitle),
+              ),
+              location,
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  widget.postContent,
+                  style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                ),
+              ),
+              postImage
+            ],
+          ),
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(color: Colors.grey[400], spreadRadius: 3, blurRadius: 10),
+          ],
+        ),
+      );
+    }
   }
 }
